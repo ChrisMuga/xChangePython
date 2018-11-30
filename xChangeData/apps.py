804b7ac192c0f0b4010e0172a68d16e845d8a4d2
Chris Muga <chrismuga94@gmail.com>
2018-11-30 04:36:08 +0300

    initial commit

---
+++ xChangeData/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class XchangedataConfig(AppConfig):
+    name = 'xChangeData'
---
+++ xChangeData/routes.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('user/', views.user, name='user'),
+    path('', views.index, name='index'),
+]
